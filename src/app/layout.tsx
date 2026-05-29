1648d7438cd8a3f3e206c499b39bcb9fe90c758a
Wallace Palmer <darthdegen@gmail.com>
2026-05-29 14:25:55 -0700

    feat: add top loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,16 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="h-dvh overflow-hidden flex flex-col">{children}</body>
+      <body className="h-dvh overflow-hidden flex flex-col">
+        <NextTopLoader
+          color="#4A9B8E"
+          height={4}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+        />
+        {children}
+      </body>
     </html>
   );
 }
